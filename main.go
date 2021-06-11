75cbbb61e8a1e7be2f6185bfd4c5e92c4cd48e1c
Bryan Ngo <bryanngo@berkeley.edu>
2021-06-10 20:27:39 -0700

    add backend flag

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 func main() {
-	isPostfix := flag.Bool("-postfix", false, "evaluate postfix instead of infix expressions")
+	isPostfix := flag.Bool("postfix", false, "evaluate postfix instead of infix expressions")
 	printTokens := flag.Bool("t", false, "print tokens from the lexer")
 	printAST := flag.Bool("a", false, "print the AST from the parser (infix only)")
+	backend := flag.String("backend", "recursive", "{recursive, shunting} backend for parsing infix expressions")
 	flag.Parse()
 
 	for {
@@ -37,15 +38,23 @@ func main() {
 		if *isPostfix {
 			res = PostfixEval(tokens)
 		} else {
-			parser := MakeParser(tokens)
-			ast, err := parser.parse()
-			if *printAST {
-				fmt.Println(ast)
+			if *backend == "shunting" {
+				pfTokens := ShuntingYard(tokens)
+				if *printAST {
+					fmt.Println(pfTokens)
+				}
+				res = PostfixEval(ShuntingYard(tokens))
+			} else if *backend == "recursive" {
+				parser := MakeParser(tokens)
+				ast, err := parser.parse()
+				if *printAST {
+					fmt.Println(ast)
+				}
+				if err != nil {
+					fmt.Println(err)
+				}
+				res = Eval(ast)
 			}
-			if err != nil {
-				fmt.Println(err)
-			}
-			res = Eval(ast)
 		}
 
 		fmt.Println(res)
